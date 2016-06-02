1b043e6a1f95331892b8bfd3d5f7c9504efcb299
Mervyn McCreight <mervyn.mccreight@gmail.com>
2016-06-02 23:07:15 +0200

    static imports

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,11 +1,14 @@
-import java.util.AbstractMap;
+import java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
+import static java.util.Comparator.naturalOrder;
+import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toMap;
 
 public class Board {
@@ -13,17 +16,17 @@ public class Board {
     private final int dimension;
 
     public static Board createEmpty(int dimension) {
-        ArrayList<Map.Entry<Coordinate, Spot>> entries = new ArrayList<>();
+        ArrayList<Entry<Coordinate, Spot>> entries = new ArrayList<>();
 
         for (int x = 0; x < dimension; x++) {
             for (int y = 0; y < dimension; y++) {
-                entries.add(new AbstractMap.SimpleImmutableEntry<>(new Coordinate(x, y), Spot.EMPTY));
+                entries.add(new SimpleImmutableEntry<>(new Coordinate(x, y), Spot.EMPTY));
             }
         }
 
         return new Board(entries
                 .stream()
-                .collect(toMap(Map.Entry::getKey, Map.Entry::getValue)),
+                .collect(toMap(Entry::getKey, Entry::getValue)),
                 dimension
         );
     }
@@ -37,18 +40,18 @@ public class Board {
         return state.entrySet()
                 .stream()
                 .filter(entry -> entry.getKey().x() == x)
-                .sorted(Comparator.comparing(entry -> entry.getKey().y(), Comparator.naturalOrder()))
-                .map(Map.Entry::getValue)
-                .collect(Collectors.toList());
+                .sorted(Comparator.comparing(entry -> entry.getKey().y(), naturalOrder()))
+                .map(Entry::getValue)
+                .collect(toList());
     }
 
     public List<Spot> column(int y) {
         return state.entrySet()
                 .stream()
                 .filter(entry -> entry.getKey().y() == y)
-                .sorted(Comparator.comparing(entry -> entry.getKey().x(), Comparator.naturalOrder()))
-                .map(Map.Entry::getValue)
-                .collect(Collectors.toList());
+                .sorted(Comparator.comparing(entry -> entry.getKey().x(), naturalOrder()))
+                .map(Entry::getValue)
+                .collect(toList());
     }
 
     public void show() {
@@ -70,8 +73,8 @@ public class Board {
         List<Coordinate> collect = state.entrySet()
                 .stream()
                 .filter(coordinateSpotEntry -> coordinateSpotEntry.getValue().equals(Spot.EMPTY))
-                .map(Map.Entry::getKey)
-                .collect(Collectors.toList());
+                .map(Entry::getKey)
+                .collect(toList());
 
         if (collect.isEmpty()) {
             return;
